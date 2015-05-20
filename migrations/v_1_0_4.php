234c7bdf1855c24779ef372217ef7e43cac4fff8
AlexSheer <alex_sheer@inbox.lv>
2015-05-20 15:04:04 +0300

    Update v_1_0_4.php

--- migrations/v_1_0_4.php
+++ migrations/v_1_0_4.php
@@ -9,33 +9,25 @@
 
 namespace bb3mobi\imgposts\migrations;
 
-class v_1_0_3 extends \phpbb\db\migration\migration
+class v_1_0_4 extends \phpbb\db\migration\migration
 {
 	public function effectively_installed()
 	{
-		return isset($this->config['imgposts_version']) && version_compare($this->config['imgposts_version'], '1.0.3', '>=');
+		return isset($this->config['imgposts_version']) && version_compare($this->config['imgposts_version'], '1.0.4', '>=');
 	}
 
 	static public function depends_on()
 	{
-		return array('\bb3mobi\imgposts\migrations\v_1_0_2');
+		return array('\bb3mobi\imgposts\migrations\v_1_0_3');
 	}
 
 	public function update_data()
 	{
 		return array(
 			// Add configs
-			array('config.add', array('imgposts_cron', '1')),
-			array('config.add', array('images_prune_last_gc', '0', '1')),
-			array('config.add', array('images_prune_gc', '172800')),
+			array('config.add', array('last_images_gallery', '0')),
 			// Current version
-			array('config.update', array('imgposts_version', '1.0.3')),
-			array('module.add', array('acp', 'ACP_IMG_FROM_POSTS', array(
-				'module_basename'	=> '\bb3mobi\imgposts\acp\imgposts_module',
-				'module_langname'	=> 'ACP_IMG_MANAGE_POSTS',
-				'module_mode'		=> 'manage_imgposts',
-				'module_auth'		=> 'ext_bb3mobi/imgposts && acl_a_board',
-			))),
+			array('config.update', array('imgposts_version', '1.0.4')),
 		);
 	}
 }
